32d597c377e350fead0553bb4faf6fe86f547291
supervueman <supervueman@gmail.com>
2020-08-22 17:25:03 +0400

    Start server

---
+++ server/routes/index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+
+// Controllers
+const controller = require('../controller');
+
+// Middleware
+const profileByAccessToken = require('../../../middleware/profileByAccessToken');
+const profileByApiKey = require('../../../middleware/profileByApiKey');
+
+/**
+ * @swagger
+ * tags:
+ *   name: PushNotifications
+ *   description: Push notifications management
+ */
+
+/**
+ * @swagger
+ * path:
+ *  /push-notifications/subscribe:
+ *    post:
+ *      summary: Subscribe to push notifications
+ *      tags: [PushNotifications]
+ *      parameters:
+ *        - in: header
+ *          name: x-api-key
+ *          required: true
+ *          schema:
+ *            type: string
+ *      requestBody:
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/PushSubscribe'
+ *      responses:
+ *        "200":
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                    type: string
+ *      security:
+ *        - basicAuth: []
+ */
+router.post('/subscribe', profileByApiKey, controller.subscribe);
+
+module.exports = router;
